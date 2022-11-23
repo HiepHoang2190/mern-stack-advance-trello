6fd7e6ff540de637fe4624636418a12379e2add3
Ku Sộp <kusop@MacBook-Air-cua-Ku.local>
2022-11-23 19:16:09 +0700

    day_eleven: upload image cloudinary

--- src/utilities/validators.js
+++ src/utilities/validators.js
@@ -11,3 +11,19 @@ export const fieldErrorMessage = (errors, fieldName) => {
     
     return <div className='auth__form__field-error'>{errors[fieldName]?.message}</div>
 }
+
+export const LIMIT_COMMON_FILE_SIZE = 10485760 // byte = 10 MB
+export const ALLOW_COMMON_FILE_TYPES = ['image/jpg', 'image/jpeg', 'image/png']
+export const singleFileValidator = (file) => {
+ if (!file || !file.name || !file.size || !file.type) {
+   return 'File cannot be blank.'
+ }
+ if (file.size > LIMIT_COMMON_FILE_SIZE) {
+   return 'Maximum file size exceeded. (10MB)'
+ }
+ if (!ALLOW_COMMON_FILE_TYPES.includes(file.type)) {
+   return 'File type is invalid.'
+ }
+ return null
+}
+
